cc5ef98f4bbc31471b5680b9807881e8c486f311
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-17 13:26:34 +0300

    solved lab

--- lab_sets.py
+++ lab_sets.py
@@ -1,9 +1,9 @@
-nestle_products = {"KitKat" : '34,456,432', 
+nestle_products :dict = {"KitKat" : '34,456,432', 
                     "Nescafe" : '14,106,132' ,
                      "Maggi" : '9,960,312' , 
                      "Nido" : '44,506,003' }
 
-unilever_products = { "Lipton" : '23,456,000' ,
+unilever_products :dict = { "Lipton" : '23,456,000' ,
                        "Breyers" : '1,235,891', 
                        "HellManns" : '17,241,412' , 
                         "Marmite" : '11,715,324' }
@@ -16,10 +16,11 @@ elif len(nestle_products) < len(unilever_products):
      print("the company has more products is :unilever_products ")
 else :
     print ("All of them are equal")
-top_selling_of_nestle=max(nestle_products)
-print(top_selling_of_nestle)
-top_selling_of_unilever= max(unilever_products)
-print(top_selling_of_unilever)
+
+top_selling_of_nestle=max(nestle_products.values())
+print("the top selling :" ,top_selling_of_nestle )
+top_selling_of_unilever= max(unilever_products.items())
+print( "the top selling :" ,top_selling_of_unilever)
 
 cities_unilever={"Saudi Arabia", "Oman", "Kuwait", "Egypt", "Jordan", "Sudan"}
 cities_nestle ={"Saudi Arabia", "Kuwait", "Iraq", "Morocco", "Yemen", "United Emirates"}
