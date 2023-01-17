187020d446a2f717ab7d6d3b40a5e3cd18ccaf5e
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-17 12:08:44 +0300

    solved lab

---
+++ lab_sets.py
@@ -0,0 +1,31 @@
+nestle_products = {"KitKat" : '34,456,432', 
+                    "Nescafe" : '14,106,132' ,
+                     "Maggi" : '9,960,312' , 
+                     "Nido" : '44,506,003' }
+
+unilever_products = { "Lipton" : '23,456,000' ,
+                       "Breyers" : '1,235,891', 
+                       "HellManns" : '17,241,412' , 
+                        "Marmite" : '11,715,324' }
+print (dict(unilever_products))
+print (dict(nestle_products))
+if len(nestle_products) > len(unilever_products):
+    print("the company has more products is : nestle_products")
+
+elif len(nestle_products) < len(unilever_products):
+     print("the company has more products is :unilever_products ")
+else :
+    print ("All of them are equal")
+top_selling_of_nestle=max(nestle_products)
+print(top_selling_of_nestle)
+top_selling_of_unilever= max(unilever_products)
+print(top_selling_of_unilever)
+
+cities_unilever={"Saudi Arabia", "Oman", "Kuwait", "Egypt", "Jordan", "Sudan"}
+cities_nestle ={"Saudi Arabia", "Kuwait", "Iraq", "Morocco", "Yemen", "United Emirates"}
+cities_of_selling_both= cities_nestle | cities_unilever
+print (cities_of_selling_both)
+intersection_cities = cities_nestle & cities_unilever 
+print (intersection_cities)
+deffrince_cities= cities_nestle - cities_unilever
+print( deffrince_cities)
