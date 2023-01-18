3f99a57c109f85787711cf6abc9d9499c1a9433e
sultan <sultan_33_886@outlook.sa>
2023-01-18 08:48:25 +0300

    lab_LAB_SETS

---
+++ LAB_SET.py
@@ -0,0 +1,17 @@
+Nestle_products={"KitKat": 34456432 ,"Nescafe" : 14106132 ,"Maggi" : 9960312 ,"Nido" : 44506003 }
+Unilever_products={"Lipton": 23456000 ,"Breyers" : 1235891 ,"HellManns" : 17241412 ,"Marmite" : 11715324 }
+Nestle={"Saudi Arabia", "Oman", "Kuwait", "Egypt", "Jordan", "Sudan"}
+Unilever={"Saudi Arabia", "Kuwait", "Iraq", "Morocco", "Yemen", "United Emirates"}
+print(Nestle_products)
+print(Unilever_products)
+print(Nestle_products.keys())
+for x in Nestle_products,Unilever_products:
+    if x!=Nestle_products and Unilever_products:
+        print((Nestle_products,Unilever_products))
+    else:
+        print("it is same roducts")
+print(max(Nestle_products))
+print(max(Unilever_products))
+for x in Nestle:
+    x=Unilever.add
+    print(Unilever )
